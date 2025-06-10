36a8b279ac43a90cd114a282af1c454843ba7bd9
ivan ugarte <ivanugartem@gmail.com>
2025-06-10 11:37:57 -0400

    add

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
-import plotly.express as px 
-import pandas as pd
+st.write("aqui a ver si pasa")
+# import plotly.express as px 
+# import pandas as pd
 
-def main():
-    df = pd.read_csv("datos_ventas.csv")
-    df_count = df.groupby("Producto").count().reset_index()
-    st.dataframe(df)  
-    fig = px.pie(df_count, values="Precio_Unitario", names="Region", title="total")
-    st.plotly_chart(fig)
+# def main():
+#     df = pd.read_csv("datos_ventas.csv")
+#     df_count = df.groupby("Producto").count().reset_index()
+#     st.dataframe(df)  
+#     fig = px.pie(df_count, values="Precio_Unitario", names="Region", title="total")
+#     st.plotly_chart(fig)
 
-main()
+# main()
+
